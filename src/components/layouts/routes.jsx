76ae48708a8e93abb72a50f31102be3317a4d122
salmaa844 <alisalmaa455@gmail.com>
2023-12-13 18:35:53 +0200

    changes

--- src/components/layouts/routes.jsx
+++ src/components/layouts/routes.jsx
@@ -39,7 +39,7 @@ export const router = createBrowserRouter([
             element:
             
               <SendCode/>
-           
+
           },{
             path:'forgetpassword',
             element:
